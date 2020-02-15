33b10944342ca61b7ace11a340ffd3e5a6db1369
Nikhil Gavali <gavalinikhil700@gmail.com>
2020-02-15 21:30:53 +0530

    modified db

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const { userRoutes } = require('./routes/user.routes');
 
+var url = process.env.MONGODB_URL;
+
+function db() {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log("connected");
+        })
+        .catch((err) => { return res.status(500).send("Crashed!!!") })
+}
+
 app.use(cors());
 app.use(db);
 app.use(bodyParser.json());
@@ -12,4 +22,5 @@ app.use('/users', userRoutes);
 
 app.listen(process.env.PORT | process.env.port | 2000, () => {
     console.log("Connected to server!!!!!");
+    db();
 })
