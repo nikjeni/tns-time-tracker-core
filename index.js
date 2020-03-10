5b5a1c6611b75bea3a064930188f93a75b1c82c4
Nikhil Gavali <gavalinikhil700@gmail.com>
2020-03-10 23:07:10 +0530

    added port

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.use('/time', timeRoutes);
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-app.listen(server_host, server_port, () => {
+app.listen(server_port, server_host, () => {
     console.log('Listening on port %d', server_port);
 })
