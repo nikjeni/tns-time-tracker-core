63e3339722ac733cc89cceed835a59ff4a0c295e
Nikhil Gavali <gavalinikhil700@gmail.com>
2020-03-14 14:30:52 +0530

    added loggers

--- controller/time.controller.js
+++ controller/time.controller.js
@@ -7,8 +7,10 @@ module.exports.saveStartTime = async (req, res, next) => {
 
     try {
         let result = await startTime.save();
+        console.log("save start time result", result);
         res.status(200).send({ status: true, message: "Start time saved successfully", result: result });
     } catch (err) {
+        console.log("save start time error", err);
         res.status(500).send("Unable to save start time");
     }
 }
@@ -18,8 +20,10 @@ module.exports.saveEndTime = async (req, res, next) => {
 
     try {
         let result = await endTime.save();
+        console.log("save end time result", result);
         res.status(200).send({ status: true, message: "End time saved successfully", result: result });
     } catch (err) {
+        console.log("save end time error", err);
         res.status(500).send("Unable to save end time");
     }
 }
@@ -30,10 +34,12 @@ module.exports.saveBreakfastTime = async (req, res, next) => {
 
     try {
         let result = await breakfasttime.save();
+        console.log("save break time result", result);
         if (result) {
             return res.status(200).send({ status: true, message: "Saved break time successfully" });
         }
     } catch (err) {
+        console.log("save break time error", err);
         return res.status(500).send("Unable to save breakfast time");
     }
 }
@@ -41,9 +47,10 @@ module.exports.saveBreakfastTime = async (req, res, next) => {
 module.exports.fetchTimeRecords = async (req, res, next) => {
     try {
         let result = await BreakfastTimeModel.find({ user_id: req.body.userId, created_at: req.body.created_at });
+        console.log("fetch break time details result", result);
         return res.status(200).send({ status: true, message: "Fetched Successfully", result: result });
     } catch (err) {
-        console.log(err);
+        console.log("fetch break time details error", err);
         return res.status(500).send("Unable to fetch records");
     }
 }
@@ -51,9 +58,10 @@ module.exports.fetchTimeRecords = async (req, res, next) => {
 module.exports.fetchStartTime = async (req, res, next) => {
     try {
         let result = await StartTimeModel.find({ user_id: req.body.userId, created_at: req.body.created_at });
+        console.log("fetch start time details result", result);
         return res.status(200).send({ status: true, message: "Fetched Successfully", result: result });
     } catch (err) {
-        console.log(err);
+        console.log("fetch start time details error", err);
         return res.status(500).send("Unable to fetch records");
     }
 }
@@ -62,9 +70,10 @@ module.exports.fetchStartTime = async (req, res, next) => {
 module.exports.fetchEndTime = async (req, res, next) => {
     try {
         let result = await EndTimeModel.find({ user_id: req.body.userId, created_at: req.body.created_at });
+        console.log("fetch end time detailsc result", result);
         return res.status(200).send({ status: true, message: "Fetched Successfully", result: result });
     } catch (err) {
-        console.log(err);
+        console.log("fetch end time details error", err);
         return res.status(500).send("Unable to fetch records");
     }
 }
